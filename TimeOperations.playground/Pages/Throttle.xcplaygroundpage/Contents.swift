27b84f35d46d65e82c72b12e70b9fd87268e1dc9
hesperhu <hesper.hu@gmail.com>
2023-03-19 07:58:44 +0800

    throttle可以对消息进行采样

--- TimeOperations.playground/Pages/Throttle.xcplaygroundpage/Contents.swift
+++ TimeOperations.playground/Pages/Throttle.xcplaygroundpage/Contents.swift
@@ -2,36 +2,103 @@ import Combine
 import SwiftUI
 import PlaygroundSupport
 
-<# Add your code here #>
+var subscriptions = [AnyCancellable]()
 
-//: [Next](@next)
 /*:
- Copyright (c) 2021 Razeware LLC
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- distribute, sublicense, create a derivative work, and/or sell copies of the
- Software in any work that is designed, intended, or marketed for pedagogical or
- instructional purposes related to programming, coding, application development,
- or information technology.  Permission for such use, copying, modification,
- merger, publication, distribution, sublicensing, creation of derivative works,
- or sale is expressly withheld.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
+throttle可以对消息进行采样 2023-03-19(Sun) 07:44:58
+*/
 
+let throttleDelay = 1.0
+/*:
+创建四个消息队列，一个消息源，两个采样消息队列，一个去抖动队列(用来对比效果) 2023-03-19(Sun) 07:45:38
+*/
+let source = PassthroughSubject<String, Never>()
+let throttle = source
+    .throttle(for: .seconds(throttleDelay), scheduler: DispatchQueue.main, latest: false)
+    .share()
+let throttleLatest = source
+    .throttle(for: .seconds(throttleDelay), scheduler: DispatchQueue.main, latest: true)
+    .share()
+let debounced = source
+    .debounce(for: .seconds(throttleDelay), scheduler: DispatchQueue.main)
+    .share()
+/*:
+UI显示四个消息队列 2023-03-19(Sun) 07:46:02
+*/
+let sourceTimeLine = TimelineView(title: "消息源")
+let throttleTimeLine = TimelineView(title: "最早节流消息")
+let throttleLatestTimeLine = TimelineView(title: "最新节流消息")
+let debounceTimeLine = TimelineView(title: "去抖动消息")
+let view = VStack{
+    sourceTimeLine
+    throttleTimeLine
+    throttleLatestTimeLine
+    debounceTimeLine
+}
+
+PlaygroundPage.current.liveView = UIHostingController(rootView: view.frame(width: 400, height: 400))
+
+source.displayEvents(in: sourceTimeLine)
+throttle.displayEvents(in: throttleTimeLine)
+throttleLatest.displayEvents(in: throttleLatestTimeLine)
+debounced.displayEvents(in: debounceTimeLine)
+/*:
+![](hsw_2023-03-19_07.54.35.png)
+*/
+
+/*:
+控制台显示消息队列 2023-03-19(Sun) 07:48:03
+*/
+
+source
+    .sink { value in
+        print("消息源(+\(deltaTime)s)：\(value)")
+    }
+    .store(in: &subscriptions)
+
+throttle
+    .sink { value in
+        print("最早节流消息(+\(deltaTime)s)：\(value)")
+    }
+    .store(in: &subscriptions)
+
+throttleLatest
+    .sink { value in
+        print("最新节流消息(+\(deltaTime)s)：\(value)")
+    }
+    .store(in: &subscriptions)
+
+debounced
+    .sink { value in
+        print("去抖动消息(+\(deltaTime)s)：\(value)")
+    }
+    .store(in: &subscriptions)
+
+source.feed(with: typingHelloWorld)
+
+/*:
+```
+ 消息源(+0.0s)：H
+ 最早节流消息(+0.0s)：H
+ 最新节流消息(+0.0s)：H
+ 消息源(+0.0s)：He
+ 消息源(+0.1s)：Hel
+ 消息源(+0.2s)：Hell
+ 消息源(+0.5s)：Hello
+ 消息源(+0.6s)：Hello
+ 最早节流消息(+1.1s)：He
+ 最新节流消息(+1.1s)：Hello
+ 去抖动消息(+1.6s)：Hello
+ 消息源(+2.0s)：Hello W
+ 消息源(+2.1s)：Hello Wo
+ 最早节流消息(+2.1s)：Hello W
+ 最新节流消息(+2.1s)：Hello Wo
+ 消息源(+2.2s)：Hello Wor
+ 消息源(+2.4s)：Hello Worl
+ 消息源(+2.5s)：Hello World
+ 最早节流消息(+3.1s)：Hello Wor
+ 最新节流消息(+3.1s)：Hello World
+ 去抖动消息(+3.5s)：Hello World
+
+ ```
+*/
