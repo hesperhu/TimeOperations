9527b780e76e460a23f0f102cada4f2578d3c9da
hesperhu <hesper.hu@gmail.com>
2023-03-19 15:48:13 +0800

    使用measureInterval(using:)来测量相邻消息的时间间隔

--- TimeOperations.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
+++ TimeOperations.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
@@ -2,35 +2,98 @@ import Combine
 import SwiftUI
 import PlaygroundSupport
 
-<# Add your code here #>
+var subscriptions = [AnyCancellable]()
 
 /*:
- Copyright (c) 2021 Razeware LLC
+使用measureInterval(using:)来测量相邻消息的时间间隔 2023-03-19(Sun) 15:44:27
+*/
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+/*:
+创建一个消息队列，两个时间测量器队列 2023-03-19(Sun) 15:45:39
+*/
 
- Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- distribute, sublicense, create a derivative work, and/or sell copies of the
- Software in any work that is designed, intended, or marketed for pedagogical or
- instructional purposes related to programming, coding, application development,
- or information technology.  Permission for such use, copying, modification,
- merger, publication, distribution, sublicensing, creation of derivative works,
- or sale is expressly withheld.
+let sourcePublisher = PassthroughSubject<String, Never>()
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
+let measureTimePublisher = sourcePublisher.measureInterval(using: DispatchQueue.main)
 
+let measureTimePublisherInRunLoop = sourcePublisher.measureInterval(using: RunLoop.main)
+
+/*:
+UI显示三个队列
+*/
+
+let sourceTimeline = TimelineView(title: "信息源")
+let measuredTimeline = TimelineView(title: "测量信息(DQ)")
+let measuredRLTimeLine = TimelineView(title: "测量信息(RL)")
+
+let view = VStack{
+    sourceTimeline
+    measuredTimeline
+    measuredRLTimeLine
+}
+
+PlaygroundPage.current.liveView = UIHostingController(rootView: view.frame(width: 300, height: 300))
+
+measureTimePublisher.displayEvents(in: measuredTimeline)
+sourcePublisher.displayEvents(in: sourceTimeline)
+measureTimePublisherInRunLoop.displayEvents(in: measuredRLTimeLine)
+
+/*:
+![](hsw_2023-03-19_15.43.26.png)
+*/
+
+/*:
+控制台显示三个队列信息 2023-03-19(Sun) 15:46:53
+*/
+sourcePublisher
+    .sink{print("+\(deltaTime)s: 源信息: \($0)")}
+    .store(in: &subscriptions)
+measureTimePublisher
+    .sink{print("+\(deltaTime)s: -->测量信息(DispQue): \(Double($0.magnitude) / 1_000_000_000.00 )")}
+    .store(in: &subscriptions)
+measureTimePublisherInRunLoop
+    .sink { value in
+        print("+\(deltaTime)s: ++>测量信息(RunLoop): \(value.magnitude)")
+    }
+    .store(in: &subscriptions)
+
+sourcePublisher.feed(with: typingHelloWorld)
+
+/*:
+```
+ +0.0s: 源信息: H
+ +0.0s: ++>测量信息(RunLoop): 0.09597897529602051
+ +0.0s: -->测量信息(DispQue): 0.096420907
+ +0.0s: 源信息: He
+ +0.0s: ++>测量信息(RunLoop): 0.0046509504318237305
+ +0.0s: -->测量信息(DispQue): 0.004575954
+ +0.1s: 源信息: Hel
+ +0.1s: ++>测量信息(RunLoop): 0.10824799537658691
+ +0.1s: -->测量信息(DispQue): 0.108253469
+ +0.2s: 源信息: Hell
+ +0.2s: ++>测量信息(RunLoop): 0.10560798645019531
+ +0.2s: -->测量信息(DispQue): 0.105619363
+ +0.4s: 源信息: Hello
+ +0.4s: ++>测量信息(RunLoop): 0.18726706504821777
+ +0.4s: -->测量信息(DispQue): 0.187276179
+ +0.5s: 源信息: Hello
+ +0.5s: ++>测量信息(RunLoop): 0.12061500549316406
+ +0.5s: -->测量信息(DispQue): 0.120597812
+ +1.9s: 源信息: Hello W
+ +1.9s: ++>测量信息(RunLoop): 1.378132939338684
+ +1.9s: -->测量信息(DispQue): 1.378142777
+ +2.1s: 源信息: Hello Wo
+ +2.1s: ++>测量信息(RunLoop): 0.2014549970626831
+ +2.1s: -->测量信息(DispQue): 0.201470327
+ +2.1s: 源信息: Hello Wor
+ +2.1s: ++>测量信息(RunLoop): 0.00014102458953857422
+ +2.1s: -->测量信息(DispQue): 0.000135757
+ +2.4s: 源信息: Hello Worl
+ +2.4s: ++>测量信息(RunLoop): 0.31730401515960693
+ +2.4s: -->测量信息(DispQue): 0.317301633
+ +2.4s: 源信息: Hello World
+ +2.4s: ++>测量信息(RunLoop): 0.00013399124145507812
+ +2.4s: -->测量信息(DispQue): 0.000142208
+ ```
+*/
